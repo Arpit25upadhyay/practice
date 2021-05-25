d52a949cffc7ca0c2e923d655312303fce2686da
Arpit Upadhyay <arpit.upadhyay01@nagarro.com>
2021-05-25 18:46:05 +0530

    JS assignment

---
+++ request.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+_EXTERNAL_URL = 'https://gist.githubusercontent.com/nitinknolder/e98126e33735531992e7e51bc4a56c02/raw/9cd69cc8c1f188d34751af01f3b219ed526261dc/menu.json'
+
+const callExternalApiUsingRequest = (callback) => {
+    request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
+        if (err) {
+            return callback(err);
+        }
+        return callback(body);
+    });
+}
+
+module.exports.callApi = callExternalApiUsingRequest;
