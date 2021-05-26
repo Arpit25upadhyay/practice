209e570e6c43e37d9d08ad383e831b685ecf27eb
Arpit Upadhyay <arpit.upadhyay01@nagarro.com>
2021-05-26 18:02:10 +0530

    issue resolve

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const apiCallFromRequest = require('./request')
-const http = require('http')
-http.createServer((req,res) =>{
-    if(req.url==="/request"){
-        apiCallFromRequest.callApi(function(response){
-            res.write(JSON(response));
-            res.end();
-        });
-    }
-}).listen(3000);
-console.log('Running');
+// const apiCallFromRequest = require('./request')
+// const http = require('http')
+// http.createServer((req,res) =>{
+//     if(req.url==="/request"){
+//         apiCallFromRequest.callApi(function(response){
+//             res.write(JSON(response));
+//             res.end();
+//         });
+//     }
+// }).listen(3000);
+// console.log('Running');
